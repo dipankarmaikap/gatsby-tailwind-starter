c5e9720356a7b64a79771d24d9947a7ea4839cce
dipankarmaikap <dipankarmaikap477@gmail.com>
2020-01-31 13:25:14 +0530

    Read me file updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   theme: {
     extend: {
-      colors: {
-        gatsby_purple: "#8a4baf",
-        tailwind_green: "#319795"
-      }
+      colors: {}
     }
   },
   variants: {},
